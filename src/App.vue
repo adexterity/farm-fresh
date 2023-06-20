77d4a86d98b8901592536ad0bf257d94be9e3164
Joseph Suberu <opensesame4me@gmail.com>
2023-06-20 18:32:24 +0100

    set up a vue project with vite and tailwind css

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import NavBar from './components/NavBar.vue'
+</script>
+
+<template>
+  <div class="container">
+    <NavBar />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  width: 80vw;
+
+  border: 1px solid black;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <nav class="bg-white h-10 flex justify-around">
+            <h2 class="logo">Farm Fresh</h2>
+            <ul class="flex">
+                <li class="active cursor-pointer"><a href="#">Home</a></li>
+                <li class="cursor-pointer"><a href="#">Locate Stores</a></li>
+                <li class="cursor-pointer"><a href="#">Catalogue</a></li>
+                <li class="cursor-pointer"><a href="#">Contact</a></li>
+            </ul>
+            <button class="btn login-btn">Login</button>
+        </nav>
+    </div>
+</template>
+
+<style></style>
