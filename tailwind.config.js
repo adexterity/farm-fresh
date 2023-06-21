01080d3ee062b441bd5f7826a3c2b21c40c14768
Joseph Suberu <opensesame4me@gmail.com>
2023-06-21 13:23:25 +0100

    created the hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,42 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+
+        primary:{
+
+          400: "#007F5F",
+          300: "#2B9348",
+          200: "#55A630",
+          100: "#80B918",
+        },
+
+        secondary:{
+          300: "#AACC00",
+          200: "#BFD200",
+          100: "#D4D700",
+        },
+
+        tertiary:{
+
+          300: "#DDDF00",
+          200: "#EEEF20",
+          100: "#FFFF3F",
+        } 
+
+          
+      },
+      fontFamily: {
+        body:['Josefin Sans']
+      },
+      fontWeight:{
+        sm: "300",
+        md: "400",
+        lg: "600",
+        xl: "700",
+      }
+    },
   },
   plugins: [],
 }
